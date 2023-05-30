c0003d08de8d9475d5cbee78a102e58644fbba2b
wlandau-lilly <will.landau@lilly.com>
2023-05-30 14:41:08 -0400

    Vectorize the likelihood

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,10 +12,10 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 
-RcppExport SEXP _rcpp_module_boot_stan_fit4model_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4historicalborrowlong_mod();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rcpp_module_boot_stan_fit4model_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4model_mod, 0},
+    {"_rcpp_module_boot_stan_fit4historicalborrowlong_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4historicalborrowlong_mod, 0},
     {NULL, NULL, 0}
 };
 
